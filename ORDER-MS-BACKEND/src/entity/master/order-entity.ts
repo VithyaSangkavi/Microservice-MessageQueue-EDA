7ff058d86a3a79ac62aab6a069512ac92e8dc584
it21096570 <it21096570@my.sliit.lk>
2024-02-05 21:20:41 +0530

    Entity

---
+++ ORDER-MS-BACKEND/src/entity/master/order-entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, Table } from "typeorm";
+import { Status } from "../../enum/status";
+import { OrderItemEntity } from "./order-items-entity";
+
+@Entity({
+  name: "order",
+})
+export class OrderEntity {
+  @PrimaryGeneratedColumn({name: "orderId"})
+  id: number;
+
+  @Column()
+  customerName: string;
+
+  @Column()
+  customerPhoneNumber: number;
+
+  @Column()
+  address: string;
+
+  @Column()
+  createdDate: Date;
+
+  @Column()
+  updatedDate: Date;
+
+  @Column()
+  total: number;
+
+  @Column({ type: "enum" ,enum:Status,default:Status.Online})
+  status: Status;
+  
+  @OneToMany(() => OrderItemEntity, (orderItem) => orderItem.order)
+  orderItem: OrderItemEntity[];
+}
---
+++ ORDER-MS-BACKEND/src/entity/master/order-items-entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, Table } from "typeorm";
+import { OrderEntity } from "./order-entity";
+
+@Entity({
+  name: "orderItem",
+})
+export class OrderItemEntity {
+  @PrimaryGeneratedColumn({name: "orderItemId"})
+  id: number;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  createdDate: Date;
+
+  @Column()
+  updatedDate: Date;
+  
+  @ManyToOne(()=> OrderEntity, (order) => order.orderItem)
+  @JoinColumn({ name: "orderId" })
+  order: OrderEntity;
+}
