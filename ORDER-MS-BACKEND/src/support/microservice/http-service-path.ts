f1adfe3077847de633465ffec2c13e13653872aa
VithyaSangkavi <vithyasangkavi@gmail.com>
2024-02-19 13:06:21 +0530

    Resolved the issues

--- ORDER-MS-BACKEND/src/support/microservice/http-service-path.ts
+++ ORDER-MS-BACKEND/src/support/microservice/http-service-path.ts
@@ -9,9 +9,6 @@ export default class HttpMSServicePath {
     static createOrder: string = "service/master/product-decrease";
     static taskCreate: string = "service/master/product-save"; 
     static orderCancellation: string = "http://localhost:4000/service/master/product-increase";
-<<<<<<< HEAD
+
     static confirmOrder: string ="http://localhost:4000/service/master/product-decrease";
 }
-=======
-}
->>>>>>> 779d94243d62e8a138be71a9acc55ef81bb10d59
